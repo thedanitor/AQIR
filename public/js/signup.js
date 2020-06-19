2a3952c0aab77d82b0d1b5b50f5fe324cd28f8ac
Dan Weikart <danmweikart@gmail.com>
2020-06-19 06:10:01 -0700

    brought files over from project_aqir

---
+++ public/js/signup.js
@@ -0,0 +1,65 @@
+  
+$(document).ready(() => {
+  // Getting references to our form and input
+  const signUpForm = $("form.signup");
+  const firstNameInput = $("input#firstNameInput");
+  const lastNameInput = $("input#lastNameInput");
+  const usernameInput = $("input#usernameInput");
+  const emailInput = $("input#emailInput");
+  const passwordInput = $("input#passwordInput");
+  const modalClose = $("button.delete");
+  const modalEl = $(".modal");
+  const modalCloseBtn = $("button.close");
+
+  signUpForm.on("submit", event => {
+    event.preventDefault();
+    const userData = {
+      first_name: firstNameInput.val().trim(),
+      last_name: lastNameInput.val().trim(),
+      user_name: usernameInput.val().trim(),
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+    };
+
+    signUpUser(
+      userData.first_name,
+      userData.last_name,
+      userData.user_name,
+      userData.email,
+      userData.password
+    );
+
+    firstNameInput.val(""),
+      lastNameInput.val(""),
+      usernameInput.val(""),
+      emailInput.val(""),
+      passwordInput.val("");
+  });
+
+  function signUpUser(first_name, last_name, user_name, email, password) {
+    $.post("/api/signup", {
+      first_name: first_name,
+      last_name: last_name,
+      user_name: user_name,
+      email: email,
+      password: password,
+    })
+      .then(() => {
+        window.location.replace("/home");
+        console.log(first_name, last_name, username, email, password);
+      })
+      .catch(handleLoginErr);
+  }
+
+  modalClose.on("click", () => {
+    modalEl.removeClass("is-active");
+  });
+
+  modalCloseBtn.on("click", () => {
+    modalEl.removeClass("is-active");
+  });
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
---
+++ routes/api-routes.js
@@ -0,0 +1,142 @@
+  
+const db = require("../models");
+const passport = require("../config/passport");
+
+module.exports = function(app) {
+  app.post("/api/login", passport.authenticate("local"), (req, res) => {
+    res.json({
+      email: req.user.email,
+      id: req.user.id,
+      first_name: req.user.first_name,
+      last_name: req.user.last_name,
+      user_name: req.user.user_name,
+    });
+  });
+
+  app.post("/api/signup", (req, res) => {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      user_name: req.body.user_name,
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch(err => {
+        res.status(401).json(err);
+      });
+  });
+
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+  app.get("/api/user_data", (req, res) => {
+    if (!req.user) {
+      res.json({});
+    } else {
+      res.json({
+        email: req.user.email,
+        id: req.user.id,
+        first_name: req.user.first_name,
+        last_name: req.user.last_name,
+        user_name: req.user.user_name,
+      });
+    }
+  });
+
+  app.get("/add-product", (req, res) => {
+    res.render("newProduct");
+  });
+
+  app.get("/api/products/:id", (req, res) => {
+    db.product
+      .findOne({
+        include: [db.User],
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then(dbProduct => {
+        res.json(dbProduct);
+      });
+  });
+  app.post("/api/addProduct", (req, res) => {
+    db.product
+      .create({
+        product_name: req.body.product_name,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        product_category: req.body.product_category,
+        product_photo: req.body.product_photo,
+        product_description: req.body.product_description,
+        UserId: req.user.id,
+      })
+      .then(res.redirect("/home"));
+  });
+
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
+
+  app.get("/home", function(req, res) {
+    db.product.findAll().then(data => {
+      const products = {
+        products: data,
+      };
+      console.log(products);
+      res.render("home", products);
+    });
+  });
+
+  app.get("/userAcct", function(req, res) {
+    db.product
+      .findAll({
+        where: {
+          UserId: req.user.id,
+        },
+        include: [db.User],
+      })
+      .then(data => {
+        const products = {
+          products: data,
+        };
+        console.log(products);
+        res.render("userAcct", products);
+      });
+  });
+  app.get("/asc", function(req, res) {
+    db.product
+      .findAll({
+        order: [["price"]],
+      })
+      .then(data => {
+        const prices = {
+          products: data,
+        };
+        res.render("home", prices);
+      });
+  });
+  app.get("/desc", function(req, res) {
+    db.product
+      .findAll({
+        order: [["price", "DESC"]],
+      })
+      .then(data => {
+        const high = {
+          products: data,
+        };
+        res.render("home", high);
+      });
+  });
+
+  app.get("/cart", function(req, res) {
+    res.render("cart");
+  });
+
+  app.get("/", function(req, res) {
+    res.render("login");
+  });
+};
